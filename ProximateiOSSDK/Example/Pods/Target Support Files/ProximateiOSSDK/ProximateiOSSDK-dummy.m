7b5e971971cb5a2f422109ca7b644e00d9577ed2
noorulain.ali <noorulain.ali@avanzasolutions.com>
2016-12-18 21:50:52 +0500

    Added supportfiles for SDK

---
+++ ProximateiOSSDK/Example/Pods/Target Support Files/ProximateiOSSDK/ProximateiOSSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ProximateiOSSDK : NSObject
+@end
+@implementation PodsDummy_ProximateiOSSDK
+@end
---
+++ ProximateiOSSDK/Example/Pods/Target Support Files/ProximateiOSSDK/ProximateiOSSDK-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "CCColorCube.h"
+#import "CCLocalMaximum.h"
+#import "Alamofire.h"
+#import "AlamofireImage.h"
+
+FOUNDATION_EXPORT double ProximateiOSSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char ProximateiOSSDKVersionString[];
+
